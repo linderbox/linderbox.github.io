03f7c73c2a5388e99b050cb114d6eb8a8fc16d62
linderbox <linda.tsan@gmail.com>
2015-08-18 00:45:18 -0700

    Updated
    html, css, & js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
-$.getJSON("http://maps.google.com/?q=" + boonDocking["results"]["Latitude"] + "," + boonDocking["results"]["Longitude"], function (responseJSON) {
-	//http://maps.google.com/?q=[lat],[long]
-  console.log(responseJSON);
+// $.getJSON("http://maps.google.com/?q=" + boonDocking["results"]["Latitude"] + "," + boonDocking["results"]["Longitude"], function (responseJSON) {
+// 	//http://maps.google.com/?q=[lat],[long]
+//   console.log(responseJSON);
+// });
+
+// $(function() {
+// //caches a jQuery object containing the header element
+//     var header = $(".fade-transparent");
+//     $(window).scroll(function() {
+//         var scroll = $(window).scrollTop();
+//         if (scroll >= 200) {
+//             header.removeClass('fade-transparent').addClass("fade-background");
+//         } else {
+//             header.removeClass("fade-background").addClass('fade-transparent');
+//         }
+//     });
+// });
+
+$(window).scroll(function() {
+// 100 = The point you would like to fade the nav in.
+  
+	if ($(window).scrollTop() > 100 ){
+    
+ 		$('.nav').addClass('show');
+    
+  } else {
+    
+    $('.nav').removeClass('show');
+    
+ 	};   	
+});
+
+$('.scroll').on('click', function(e){		
+		e.preventDefault()
+    
+  $('html, body').animate({
+      scrollTop : $(this.hash).offset().top
+    }, 1500);
 });
